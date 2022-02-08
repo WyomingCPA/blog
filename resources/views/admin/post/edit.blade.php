f1bea6915725b253f7fc11b63a1983f6bb99f79b
WyomingCPA@yandex.ru <WyomingCPA@yandex.ru>
2022-02-08 17:22:12 +0300

    in view edit post add several plugins

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -4,7 +4,7 @@
 <script type="text/javascript">
     tinymce.init({
         selector: '#content',
-        plugins: "codesample",
+        plugins: "codesample, toc, autosave, wordcount",
         height : "680"
     });
 </script>

ba7e3ce3614a80881f9b5694c6f909ff82abb2d9
WyomingCPA@yandex.ru <WyomingCPA@yandex.ru>
2022-02-13 05:33:41 +0300

    update vuew

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -93,6 +93,12 @@
                 background-color: #d2f4ea;
             }
         </style>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/default.min.css">
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/styles/vs2015.min.css">
+        <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js"></script>
+        <script>
+            hljs.highlightAll();
+        </script>
     </head>
     <script>
         window.Laravel = {
